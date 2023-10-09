b0be8ba21ce7e6b6e2c14750efe29ff54c2def3f
paultasov <hipaultan@gmail.com>
2023-10-09 10:30:19 +0300

    fix (Cli.php, Engine.php): update welcome message

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,9 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function sayHello()
+function sayHello(): void
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $userName = prompt('May I have your name?');
     line("Hello, %s!", $userName);
-
-    return $userName;
 }
