61ed4f3d8272af705b4013921f2eed09cd54e066
paultasov <hipaultan@gmail.com>
2023-10-09 09:54:31 +0300

    refactor (Calc.php): update file

--- src/apps/Calc.php
+++ src/apps/Calc.php
@@ -40,7 +40,6 @@ function calculate(int $minVal, int $maxVal, int $gameAttempts): void
         askTheQuestion("$firstRandNum $randOperator $secondRandNum");
 
         $validAnswer = doMathOperation($randOperator, $firstRandNum, $secondRandNum);
-        print_r("Hint: '$validAnswer'\n");
         $userAnswer = (int) getUserAnswer();
         $answers = [$validAnswer, $userAnswer];
 
